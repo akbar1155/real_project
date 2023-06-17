5f7f15ca520df89f8e95d9eea637e464f6a5977f
Abdulqodir <your@mail.here>
2023-06-17 12:02:23 +0500

    dvevr

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,12 +1,19 @@
 import "../intro/intro.css";
-export function Intro (){
-    return <div className="intro">
-        <section className="intro__section">
-            <div className="container">
-                <h1>hello world</h1>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur repellat quaerat perspiciatis a totam esse natus nobis corporis reprehenderit facilis eligendi officiis, mollitia, laborum eum ab dolores delectus ducimus nihil.</h1>
-
-            </div>
-        </section>
+export function Intro() {
+  return (
+    <div className="intro">
+      <section className="intro__section">
+        <div className="container">
+          <div className="text__information">
+            <h1>Find the Best Courses and Amazing Mentor</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus{" "}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
+  );
 }
