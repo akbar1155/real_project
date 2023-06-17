a076d65a52a7e9ab32a65e45fde44d368b5bbef2
Abdulqodir <your@mail.here>
2023-06-17 11:54:17 +0500

    Todo list

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,12 @@
+import "../intro/intro.css";
+export function Intro (){
+    return <div className="intro">
+        <section className="intro__section">
+            <div className="container">
+                <h1>hello world</h1>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur repellat quaerat perspiciatis a totam esse natus nobis corporis reprehenderit facilis eligendi officiis, mollitia, laborum eum ab dolores delectus ducimus nihil.</h1>
+
+            </div>
+        </section>
+    </div>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Header } from './components/header/header'
+import { Intro } from './components/intro/intro'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Intro />
   </React.StrictMode>,
 )
