8d0afe085276a0687b7f2511ac128fde9955ca52
Anvarjon15 <mirzarayimovanvarjon@gmail.com>
2023-06-19 16:54:43 +0500

    jdjdbdjd

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+// import React from 'react'
+import './Footer.css'
+import LogoIpsum from '../images/Logo.svg'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className='main__links'>
+        <div>
+        <img src={LogoIpsum}/> 
+      <p style={{maxWidth:"400px"}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente incidunt voluptatibus neque distinctio enim quidem, explicabo illo consectetur error ut rerum iure eaque odit ad similique. Ab recusandae architecto placeat!</p>
+        </div>
+        <div className='footer__links'>
+        <div className='footer__link'>
+          <h3>About</h3>
+          <a href='#'>About Us</a>
+          <a href='#'>Courses</a>
+          <a href='#'>Mentor</a>
+          <a href='#'>Contact Us</a>
+        </div>
+        <div className='footer__link'>
+          <h3>Support</h3>
+          <a href='#'>Careers</a>
+          <a href='#'>Help & Support</a>
+          <a href='#'>Blog</a>
+        </div>
+        <div className='footer__link'>
+          <h3>Company</h3>
+          <a href='#'>Privacy Policy</a>
+          <a href='#'>Terms of Service</a>
+          <a href='#'>Code of COnduct</a>
+        </div>
+        </div>
+
+      </div>
+        <div className='btm'>
+        <div className='prv'>
+        <a href='#'>Privacy Policy</a>
+        </div>
+        <div className='reserv'>
+        <p>©2023 Tashkent...All Rights Reserved</p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Footer from './components/footer/Footer'
 import { Header } from './components/header/header'
 import { Intro } from './components/intro/intro'
 
@@ -7,5 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Intro />
+    <Footer/>
   </React.StrictMode>,
 )
