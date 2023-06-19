c6f3c2650814619af75acdb9d4792a0b890e6c12
Akbar <isroilovakbar49@gmail.com>
2023-06-20 00:32:20 +0500

    gdg

--- src/components/Homelinkpages/homelink.jsx
+++ src/components/Homelinkpages/homelink.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "../Homelinkpages/linkhome.scss";
+import { BiChevronRight } from "react-icons/bi";
 
 const Homelink2 = () => {
   return (
-    <div>
-      <a href="/ProgrammingCourses">Home</a>
-      <p>/</p>
-      {/* <a href="ProgrammingCourses">Programming Courses</a> */}
+    <div className="flex1 homelink container ">
+      <a href="/" className="hommelink__a1">
+        Home
+      </a>
+      <a href="/ProgrammingCourses" className="hommelink__p1">
+        <BiChevronRight />
+      </a>
     </div>
   );
 };
--- src/components/Homelinkpages/homelink1.jsx
+++ src/components/Homelinkpages/homelink1.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "../Homelinkpages/linkhome.scss";
+import { BiChevronRight } from "react-icons/bi";
 
 const Homelink1 = () => {
   return (
-    <div>
-      <a href="/">Home</a>
-      <p>/</p>
-      <a href="/detail">Programming Courses</a>
+    <div className="flex1 homelink container ">
+      <a href="/" className="hommelink__a1">
+        Home
+      </a>
+      <a href="/ProgrammingCourses" className="hommelink__p1">
+        <BiChevronRight />
+      </a>
+      <a href="/detail" className="hommelink__a2">
+        Programming Courses
+      </a>
+      <a href="/detail" className="hommelink__p1">
+        <BiChevronRight />
+      </a>
     </div>
   );
 };
