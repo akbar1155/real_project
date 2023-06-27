a262d6a3581889e856f244d5731212027d1039a0
Akbar <isroilovakbar49@gmail.com>
2023-06-27 14:55:50 +0500

    hgghg

--- src/pages/Blog/blogpages.jsx
+++ src/pages/Blog/blogpages.jsx
@@ -5,9 +5,17 @@ import { Header } from "../../components/header/header";
 
 const Blogpages = () => {
   return (
-    <div className="container">
+    <div>
       <Header />
-      <h1>blog</h1>
+      <div className="container2">
+        <h1>blog</h1>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ullam
+          quasi, quisquam eum ea corrupti temporibus cupiditate, tempore id
+          perferendis quidem, veniam consectetur. Non dolor quis quibusdam
+          repellendus tempora provident?
+        </h1>
+      </div>
       <Footer />
     </div>
   );
